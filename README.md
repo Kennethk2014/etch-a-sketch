bbee1c79967adaa008d363a8a05952c7dc0437af
Kenneth Kristiansen <kennethk2014@gmail.com>
2020-12-07 18:54:02 +0100

    Trying to make the mouseover work. obly works on a fresh page

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # etch-a-sketch
+
+    03/12-2020
+    
+This should work as a easy almost like 8-bit sketch/drawer. Wehn you mouse over a given area, fields within that area are going to change color.
+
+You should be able to pick between some colors.
+
+    - All Black
+    - Rainbow (completly random colors)
+
---
+++ scripts/index-script.js
@@ -0,0 +1,68 @@
+// HTML OBJECTS
+const gridContainer = document.querySelector('#grid-container');
+const button16 = document.querySelector('.button16');
+const button32 = document.querySelector('.button32');
+const button64 = document.querySelector('.button64');
+
+// CREATES ALL THE DIVS WHEN PAGE IS LOADED
+for(let i = 0; i < 256; i++){
+    const createDiv = document.createElement('div')
+    createDiv.classList.add('inner-div');
+    gridContainer.appendChild(createDiv)
+};
+
+
+button16.addEventListener('click', grid16)
+button32.addEventListener('click', grid32)
+button64.addEventListener('click', grid64)
+
+function grid16(){
+    gridContainer.innerHTML = "";
+
+    for(let i = 0; i < 256; i++){
+        
+        gridContainer.style.gridTemplateColumns =  "repeat(16, 1fr)";
+        gridContainer.style.gridTemplateRows =  "repeat(16, 1fr)";
+        const createDiv = document.createElement('div')
+        createDiv.classList.add('inner-div');
+        gridContainer.appendChild(createDiv)
+        
+    };
+}
+
+
+function grid32(){
+    gridContainer.innerHTML = "";
+
+    for(let i = 0; i < 1024; i++){
+        
+        gridContainer.style.gridTemplateColumns =  "repeat(32, 1fr)";
+        gridContainer.style.gridTemplateRows =  "repeat(32, 1fr)";
+        const createDiv = document.createElement('div')
+        createDiv.classList.add('inner-div');
+        gridContainer.appendChild(createDiv)
+        
+    };
+}
+
+function grid64(){
+    gridContainer.innerHTML = "";
+
+    for(let i = 0; i < 4096; i++){
+        const createDiv = document.createElement('div')
+        createDiv.classList.add('inner-div');
+        gridContainer.appendChild(createDiv)
+        gridContainer.style.gridTemplateColumns =  "repeat(64, 2fr)";
+        gridContainer.style.gridTemplateRows =  "repeat(64, 2fr)";
+
+    }
+};
+
+
+const div = document.querySelectorAll('.inner-div').forEach(item => {
+    item.addEventListener('mouseover', colorChanger)
+})
+
+function colorChanger(event){
+    event.target.classList.replace('inner-div', 'new-color')
+}
