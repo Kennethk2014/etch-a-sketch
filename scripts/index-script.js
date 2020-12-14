dfdd47c604165c59b81bb8972d93b0a1070f874e
Kenneth Kristiansen <kennethk2014@gmail.com>
2020-12-14 17:20:41 +0100

    Added slider, everything works

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,105 +1,42 @@
 // HTML OBJECTS
-const gridContainer = document.querySelector('#grid-container');
-const button16 = document.querySelector('.button16');
-const button32 = document.querySelector('.button32');
-const button64 = document.querySelector('.button64');
+const gridContainer = document.querySelector("#grid-container");
+const button16 = document.querySelector(".button16");
+const button32 = document.querySelector(".button32");
+const button64 = document.querySelector(".button64");
 
 // CREATES ALL THE DIVS WHEN PAGE IS LOADED
-for(let i = 0; i < 256; i++){
-    const createDiv = document.createElement('div')
-    createDiv.classList.add('inner-div');
-    gridContainer.appendChild(createDiv)
+for (let i = 0; i < 256; i++) {
+  const createDiv = document.createElement("div");
+  createDiv.classList.add("inner-div");
+  gridContainer.appendChild(createDiv);
+}
+
+const gridLayoutSlider = document.querySelector("#grid-layout-slider");
+
+// Makes the slider interactive and fills out the box with the user inputted divs
+gridLayoutSlider.oninput = function () {
+  let gridVal = this.value;
+  let gridArea = gridVal * gridVal;
+
+  gridContainer.innerHTML = "";
+
+  for (let i = 0; i < gridArea; i++) {
+    gridContainer.style.gridTemplateColumns = `repeat(${gridVal}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridVal}, 1fr)`;
+    const createDiv = document.createElement("div");
+    createDiv.classList.add("inner-div");
+    gridContainer.appendChild(createDiv);
+  }
+  const div = document.querySelectorAll(".inner-div").forEach((item) => {
+    item.addEventListener("mouseover", colorChanger);
+  });
 };
 
+const div = document.querySelectorAll(".inner-div").forEach((item) => {
+  item.addEventListener("mouseover", colorChanger);
+});
 
-/*button16.addEventListener('click', grid16)
-button32.addEventListener('click', grid32)
-button64.addEventListener('click', grid64)*/
-
-/*function grid16(){
-    gridContainer.innerHTML = "";
-
-    for(let i = 0; i < 256; i++){
-        
-        gridContainer.style.gridTemplateColumns =  "repeat(16, 1fr)";
-        gridContainer.style.gridTemplateRows =  "repeat(16, 1fr)";
-        const createDiv = document.createElement('div')
-        createDiv.classList.add('inner-div');
-        gridContainer.appendChild(createDiv)
-        
-    };
-    const div = document.querySelectorAll('.inner-div').forEach(item => {
-        item.addEventListener('mouseover', colorChanger)
-    })
-}*/
-
-
-function grid32(){
-    gridContainer.innerHTML = "";
-
-    for(let i = 0; i < 1024; i++){
-        
-        gridContainer.style.gridTemplateColumns =  "repeat(32, 1fr)";
-        gridContainer.style.gridTemplateRows =  "repeat(32, 1fr)";
-        const createDiv = document.createElement('div')
-        createDiv.classList.add('inner-div');
-        gridContainer.appendChild(createDiv)
-        
-    };
-    const div = document.querySelectorAll('.inner-div').forEach(item => {
-        item.addEventListener('mouseover', colorChanger)
-    })
+//Replaces the existing class with another class created in the CSS file, here i can change the colors
+function colorChanger(event) {
+  event.target.classList.replace("inner-div", "new-color");
 }
-
-function grid64(){
-    gridContainer.innerHTML = "";
-
-    for(let i = 0; i < 4096; i++){
-        const createDiv = document.createElement('div')
-        createDiv.classList.add('inner-div');
-        gridContainer.appendChild(createDiv)
-        gridContainer.style.gridTemplateColumns =  "repeat(64, 2fr)";
-        gridContainer.style.gridTemplateRows =  "repeat(64, 2fr)";
-
-    }
-    const div = document.querySelectorAll('.inner-div').forEach(item => {
-        item.addEventListener('mouseover', colorChanger)
-    })
-};
-
-const div = document.querySelectorAll('.inner-div').forEach(item => {
-    item.addEventListener('mouseover', colorChanger)
-})
-
-function colorChanger(event){
-    event.target.classList.replace('inner-div', 'new-color')
-}
-
-
-const gridLayoutSlider = document.querySelector("#grid-layout-slider")
-
-gridLayoutSlider.oninput = function(){
-    let gridVal = this.value;
-    let gridArea = gridVal * gridVal;
-
-    gridContainer.innerHTML = "";
-
-    for(let i = 0; i < gridArea; i++){
-        
-        gridContainer.style.gridTemplateColumns =  `repeat(${gridVal}, 1fr)`;
-        gridContainer.style.gridTemplateRows =  `repeat(${gridVal}, 1fr)`;
-        const createDiv = document.createElement('div')
-        createDiv.classList.add('inner-div');
-        gridContainer.appendChild(createDiv)
-        
-    };
-    const div = document.querySelectorAll('.inner-div').forEach(item => {
-        item.addEventListener('mouseover', colorChanger)
-    })
-    console.log(gridArea)
-}
-    
-    
-    
-
-
